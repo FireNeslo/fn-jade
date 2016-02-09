3070166b90d5be431429db1d1b2d9a33eda1c1b4
Øystein Ø. Olsen <fireneslo@gmail.com>
2016-02-09 22:23:59 +0100

    typecheck

--- runtime.js
+++ runtime.js
@@ -4,13 +4,21 @@
   (global.vJade = factory(global.virtualDom));
 }(this, function (virtualDom) { 'use strict';
 
+  var babelHelpers = {};
+  babelHelpers.typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
+    return typeof obj;
+  } : function (obj) {
+    return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj;
+  };
+  babelHelpers;
+
   function element(tag, attributes, children) {
     if (!Array.isArray(children)) {
       children = [children];
     }
     return new virtualDom.VNode(tag, { attributes: attributes }, children.map(function (node) {
       if (node == null) return new virtualDom.VText('');
-      return typeof node === 'string' ? new virtualDom.VText(node) : node;
+      return (typeof node === 'undefined' ? 'undefined' : babelHelpers.typeof(node)) !== 'object' ? new virtualDom.VText(node) : node;
     }));
   }
 
--- src/runtime.js
+++ src/runtime.js
@@ -6,6 +6,6 @@ export default function element(tag, attributes, children) {
   }
   return new VNode(tag, {attributes}, children.map( node => {
     if(node == null) return new VText('')
-    return typeof node === 'string' ? new VText(node) : node
+    return typeof node !== 'object' ? new VText(node) : node
   }))
 }
